c6ba9a900a15f810730ff868e620f2b4f7e2c6e4
Sharma, Neeraj <Neeraj.Sharma@garmin.com>
2019-12-30 22:26:08 -0600

    Initial Commit

---
+++ MeriList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MeriList
+//
+//  Created by Sharma, Neeraj on 12/30/19.
+//  Copyright © 2019 Sharma, Neeraj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
